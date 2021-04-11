a7567a45a1884274f3c36c180935b84d4ef74842
Sparkeagle2000 <tonnie_hovanec@yahoo.com>
2021-04-11 14:17:18 -0400

    got rid of scripts

--- Assets/Tonnie's Scripts/EnemySpot.cs
+++ Assets/Tonnie's Scripts/EnemySpot.cs
@@ -4,41 +4,46 @@ using UnityEngine;
 
 public class EnemySpot : MonoBehaviour
 {
+    TimeManager timemanager;
     public Transform player;
-    float speed=6.0f;
+    float speed = 6.0f;
     bool playerSighted;
     // Start is called before the first frame update
     void Start()
     {
-        playerSighted=false;
+        timemanager = GameObject.FindGameObjectWithTag("TimeManager").GetComponent<TimeManager>();
+        playerSighted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(playerSighted)
-        PlayerFound();
+        if (playerSighted)
+            PlayerFound();
     }
 
     void OnTriggerStay(Collider other)
     {
-        if(other.tag=="Player")
+        if (other.tag == "Player")
         {
-            playerSighted=true;
+            playerSighted = true;
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other.tag=="Player")
-        playerSighted=false;
+        if (other.tag == "Player")
+            playerSighted = false;
     }
 
     void PlayerFound()
     {
-        Vector3 lookAtPos=new Vector3(player.position.x,player.position.y,player.position.z);
-        lookAtPos.y=transform.position.y;
-        transform.LookAt(lookAtPos);
-        transform.position+=transform.forward*speed*Time.deltaTime;
+        if (!timemanager.TimeIsStopped)
+        {
+            Vector3 lookAtPos = new Vector3(player.position.x, player.position.y, player.position.z);
+            lookAtPos.y = transform.position.y;
+            transform.LookAt(lookAtPos);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
     }
 }

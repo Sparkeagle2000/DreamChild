6351b6db4a71f8ba090a2b17e4c647e4be16e9c6
Yurrushia <66466727+Yurrushia@users.noreply.github.com>
2021-04-12 19:08:28 -0400

    --- Auto-Git Commit ---

---
+++ Assets/AttackScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+
+    public Animator animator;
+
+    public Transform attackPoint;
+
+    public float attackRange = 0.5f;
+
+    public LayerMask enemyLayers;
+
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+
+    float nextAttackTime = 0f;
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time >= nextAttackTime) {
+            if (Input.GetKeyDown (KeyCode.Q)) {
+                Attack ();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+
+    }
+
+    void Attack () {
+        animator.SetTrigger ("Attack");
+
+        Collider[] hitEnemies = Physics.OverlapSphere (attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider enemy in hitEnemies) {
+            enemy.GetComponent<EnemyHP> ().TakeDamage (attackDamage);
+        }
+
+    }
+
+    void OnDrawGizmosSelected () {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere (attackPoint.position, attackRange);
+    }
+
+}
---
+++ Assets/EnemyHP.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour {
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start () {
+        currentHealth = maxHealth;
+
+    }
+
+    public void TakeDamage (int damage) {
+        currentHealth -= damage;
+
+        animator.SetTrigger ("Hurt");
+        if (currentHealth <= 0) {
+            Die ();
+        }
+    }
+
+    void Die () {
+        Debug.Log ("Enemy Died!");
+        animator.SetBool ("IsDead", true);
+        GetComponent<Collider> ().enabled = false;
+        this.enabled = false;
+    }
+}
